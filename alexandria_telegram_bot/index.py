74bc10308970f0d37a3327b7f823b7c98465b040
Lobo <matheuscardoso1@poli.ufrj.br>
2021-11-03 20:40:19 -0300

    made basic flow for receiving messages, commands and sending files

---
+++ alexandria_telegram_bot/index.py
@@ -0,0 +1,36 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.update import Update
+
+load_dotenv(find_dotenv())
+token = os.getenv('TELEGRAM_API_TOKEN')
+
+print('token:', token)
+
+#bot = telegram.Bot(token=token)
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)    
+dispatcher.add_handler(echo_handler)
+
+def book(update: Update, context):
+    #book_asked = context.args[0]
+    file = 'assets/demo.pdf'
+    update.message.reply_document(document=open(file, 'rb'))
+book_handler = CommandHandler('book', book)
+dispatcher.add_handler(book_handler)
+
+updater.start_polling()
